faaca29e6669e5788293559fa1ce8b5e24ebc7d0
David Ra Youssef <davidragraphics@gmail.com>
2015-10-20 18:03:48 -0400

    Added tldr in readme

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # funcifyr.js 
-funcifyr.js (pronounced Funkifier) is partly a joke on the *ify* suffix added everywhere, partly a resource for myself to reference often-used functions, and partly a tool that might have some use for somebody.
+funcifyr.js (pronounced Funkifier) is partly a joke on the *ify* suffix used everywhere, partly a resource for myself to reference oft-used functions, and partly a tool that might have some use for somebody.
+
+# tl;dr
+// converts NodeList into an Array
+funcifyr.arrayify()
+
+// returns random hex color
+funcifyr.colorify()
+
+// creates function from two functions
+funcifyr.composify()
+
+// creates copy of a function with preset first parameter
+funcifyr.currify()
+
+// turns function into a method  
+funcifyr.defuncify()
+
+// increases HTML5 video or audio speed
+funcifyr.fastify()
+
+// returns array prefilled with a value
+funcifyr.fillify()
+
+// flattens multidimensional arrays
+funcifyr.flattify()
+
+// turns method into a function
+funcifyr.funcify()
+
+// maps over unmappable collection
+funcifyr.mapify()
+
+// returns random integer 
+funcifyr.randomify()
+
+// repeats a string a number of times
+funcifyr.repeatify()
+
+// a more general-purpose currify
+funcifyr.schonfinkelify()
+
+// decreases HTML5 video or audio speed
+funcifyr.slowify()
+
+// removes dupicates
+funcifyr.uniqify()
+
+# TODO
+
+Instructions and examples to be added soon.
--- funcifyr.js
+++ funcifyr.js
@@ -3,15 +3,15 @@
 
   window.funcifyr = {
 
+    // converts an Array-like object, HTMLCollection or NodeList that's not mappable into an Array
     arrayify: function(collection) {
-    // Converts Array-like object, HTMLCollection or NodeList that's not mappable into Array
       return Array.from ? Array.from(collection) : Array.apply(null, collection).map(function(v) {
         return v;
       });
     },
 
-    colorify: function() {
     // returns a random hex color
+    colorify: function() {
       return '#' + (
         '0123456789ABCDEF'.split('').map(function(v,i,a) {
           return a[Math.floor(Math.random() * 16)];
@@ -19,73 +19,85 @@
       );
     },
 
-    composify: function(fn1, fn2) {
     // creates a function from two functions
+    composify: function(fn1, fn2) {
       return function composified() {
         return fn1.call(this, fn2.apply(this, arguments));
       }
     },
 
-    currify: function(fn, a) {
     // creates copy of function with preset first parameter
+    currify: function(fn, a) {
       return function currified(b) {
         return fn.call(this, a, b);
       }
     },
 
-    defuncify: function(fn) {
     // turns a function into a method  
+    defuncify: function(fn) {
       return function defuncified(a, b) {
         return fn.call(this, a, b);
       }
     },
 
+    // increases HTML5 video or audio speed by 0.1
     fastify: function(mediaElementId) {
-    // increases HTML5 video or audio speed by .1
       document.getElementById(mediaElementId).playbackRate += 0.1;
     },
 
-    funcify: function(obj, methodString) {
+    // returns an array prefilled with a value a number of times
+    fillify: function(value, times) {
+      return Array.apply(null, Array(+times)).map(function() { return value });
+    },
+
+    // Takes any number of arguments and multidimensional arrays and returns new array with results flattened
+    flattify: function(){
+      return [].slice.call(arguments).reduce(function(a, b){              
+        return a.concat(Array.isArray(b) ? funcifyr.flattify.apply(null, b) : b);
+      }, []);
+    },
+
     // turns a method into a function
+    funcify: function(obj, methodString) {
       return function funcified(a, b) {
         return obj[methodString].call(obj, a, b);
       }
     },
 
-    mapify: function(collection, callback) {
     // maps over an unmappable Array-like collection and runs a callback
+    mapify: function(collection, callback) {
       return Array.apply(null, collection).map(function(v) {
         return callback ? callback(v) : v;
       });
     },
 
-    randomify: function(min, max) {
     // returns random integer between min and max 
+    randomify: function(min, max) {
       return Math.floor(Math.random() * (max - min + 1) + min);
     },
 
-    repeatify: function(str, times) {
     // repeats a string a number of times
+    repeatify: function(str, times) {
       return Array.apply(null, new Array(times)).map(function() {
         return str;
       }).join('');
     },
 
+    // a more general-purpose currify with arbitrary arity
     schonfinkelify: function(fn) {
-    // a more general-purpose currify for arbitrary arity
       var slice = Array.prototype.slice, args = slice.call(arguments, 1);
       return function schonfinkeliied() {
         return fn.apply(null, args.concat(slice.call(arguments)));
       }
     },
 
+    // decreases HTML5 video or audio speed by 0.1
     slowify: function(mediaElementId) {
-    // decreases HTML5 video or audio speed by .1
       document.getElementById(mediaElementId).playbackRate -= 0.1;
     },
 
+    // takes an array with duplicates and returns a new one with all dupes removed
     uniqify: function(arr) {
-    // takes an array and returns a new set with all duplicates removed
       return arr.filter(function(v,i,a) {
         return i === a.indexOf(v);
       });
