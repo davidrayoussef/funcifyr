60504307dbf85b4feb5fe030c9c0a4be306f267c
David Ra Youssef <davidradeveloper@gmail.com>
2016-03-22 17:26:18 -0400

    Rename function and clean up

--- funcifyr.js
+++ funcifyr.js
@@ -8,21 +8,28 @@
       // runs two predicate functions on an argument and returns true if both are true 
       andify: function(fn1, fn2) {
         return function andified(arg) {
-          return fn1.call(this, arg) && fn2.call(this, arg);
+          return fn1.call(null, arg) && fn2.call(null, arg);
         }
       },
 
       // converts an Array-like object, HTMLCollection or NodeList that's not mappable into an Array
       arrayify: function(collection) {
-        return Array.from ? Array.from(collection) : Array.apply(null, collection).map(function(v) {
-          return v;
-        });
+        return Array.from ? Array.from(collection) : Array.apply(null, collection);
+      },
+
+      // returns a random hex color
+      colorify: function() {
+        return '#' + (
+          '0123456789ABCDEF'.split('').map(function(v,i,a) {
+            return a[Math.floor(Math.random() * 16)];
+          }).join('').slice(0,6)
+        );
       },
 
       // creates a function from two functions
       composify: function(fn1, fn2) {
         return function composified() {
-          return fn1.call(this, fn2.apply(this, arguments));
+          return fn1.call(null, fn2.apply(null, arguments));
         }
       },
 
@@ -44,13 +51,13 @@
       // creates a negate function that returns true if result is false
       falsify: function(fn) {
         return function falsified() {
-          return !fn.apply(this, arguments);
+          return !fn.apply(null, arguments);
         }
       },
 
       // returns an array prefilled with a value a number of times
       fillify: function(value, times) {
-        return Array.apply(null, Array(+times)).map(function() { 
+        return Array.fill ? new Array(times).fill(value) : Array.apply(null, Array(+times)).map(function() { 
           return value;
         });
       },
@@ -86,15 +93,6 @@
         };
       },
 
-      // returns a random hex color
-      hexify: function() {
-        return '#' + (
-          '0123456789ABCDEF'.split('').map(function(v,i,a) {
-            return a[Math.floor(Math.random() * 16)];
-          }).join('').slice(0,6)
-        );
-      },
-      
       // creates predicate function to test for numbers less than x
       lessthanify: function(x) {
         return function lessthanified(y) {
@@ -119,21 +117,21 @@
       // runs two predicate functions on an argument, returns true if one OR the other is true 
       orify: function(fn1, fn2) {
         return function orified(arg) {
-          return fn1.call(this, arg) || fn2.call(this, arg);
+          return fn1.call(null, arg) || fn2.call(null, arg);
         }
       },
 
       // creates a copy of a function with a preset first parameter
       partialify: function(fn, a) {
         return function partialified(b) {
-          return fn.call(this, a, b);
+          return fn.call(null, a, b);
         }
       },
 
       // runs a function on the passed in results of another function
       pipeify: function(fn1, fn2) {
         return function pipeified() {
-          return fn2.call(this, fn1.apply(this, arguments));
+          return fn2.call(null, fn1.apply(null, arguments));
         }
       },
 
@@ -174,7 +172,7 @@
 
       // takes an array with duplicates and returns a new one with all dupes removed
       uniqify: function(arr) {
-        return arr.filter(function(v,i,a) {
+        return Array.from ? Array.from(new Set(arr)) : arr.filter(function(v,i,a) {
           return i === a.indexOf(v);
         });
       },
