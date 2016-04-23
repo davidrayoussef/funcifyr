08e89f925f4b0933ac67265b38604ffdd2a148b6
David Ra <davidradeveloper@gmail.com>
2016-04-23 13:32:38 -0400

    Convert tests to es2015

--- tests/funcifyr.test.js
+++ tests/funcifyr.test.js
@@ -1,38 +1,48 @@
-var F = require('../funcifyr.js').F;
-var expect = require('chai').expect;
+import F from '../funcifyr.js';
+import {expect, assert} from 'chai';
 
-describe('andify', function() {
-  it('should return true if arg passed to pred funcs evaluates to true', function() {
+describe('andify', () => {
+  it('should return true if arg passed to pred funcs evaluates to true', () => {
 
-    function isString(a) { return typeof a === 'string'; };
-    function isLongerThanSix(b) { return b.length > 6; };
-    var testIfStringANDOverSix = F.andify(isString, isLongerThanSix)('funcify all the things');
+    let isString = (a) => typeof a === 'string';
+    let isLongerThanSix = (b) => b.length > 6;
+    let testIfStringANDOverSix = F.andify(isString, isLongerThanSix)('funcify all the things');
 
     expect(testIfStringANDOverSix).to.be.true;
 
   });
 });
 
-describe('arrayify', function() {
-  it('should turn an array-like collection into a real array', function() {
+describe('arrayify', () => {
+  it('should turn an array-like collection into a real array', () => {
 
     // Can't test a DOM collection so we'll run test on arguments object
-    function returnArgs() { return arguments; }
-    var argumentsObjectToArray = F.arrayify(returnArgs('string', 3));
-    var testIfArray = Array.isArray(argumentsObjectToArray);
+    let returnArgs = () => arguments;
+    let argumentsObjectToArray = F.arrayify(returnArgs('string', 3));
+    let testIfArray = Array.isArray(argumentsObjectToArray);
 
     expect(testIfArray).to.be.true;
 
   });
 });
 
-describe('colorify', function() {
-  it('should return a random 6 character hex color', function() {
+describe('colorify', () => {
+  it('should return a random 6 character hex color', () => {
 
-    var isHexColor = /^#([A-Fa-f0-9]{6})$/;
-    var testIfHexColor = isHexColor.test(F.colorify());
+    let isHexColor = /^#([A-Fa-f0-9]{6})$/;
+    let testIfHexColor = isHexColor.test(F.colorify());
 
     expect(testIfHexColor).to.be.true;
 
+  });
+});
+
+describe('composify', () => {
+  it('should return a function', () => {
+
+    let actual = typeof F.composify();
+    let expected = 'function';
+    
+    assert.equal(actual, expected);
   });
 });
